1582dc20afaeac759aa799183558c555e171ded4
iviisaamm <83715305+iviisaamm@users.noreply.github.com>
2021-06-18 17:51:15 -0300

    Add files via upload

---
+++ TP Nº1- AyED/ejercicio_3a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------------------------------------------" << endl;
+    cout << "| EJERCICIO 3.a - Conversion de m/s a km/h                           |" << endl;
+    cout << "---------------------------------------------------------------------" << endl;
+    int velocidad_mxs = 0;
+    int velocidad_kxh = 0;
+    cout << "Ingrese Velocidad en m/h: ";
+    cin >> velocidad_mxs;
+    velocidad_kxh = velocidad_mxs * 3.6;
+    cout << "La velocidad en Km/s es: " << velocidad_kxh << endl;
+
+
+    }
